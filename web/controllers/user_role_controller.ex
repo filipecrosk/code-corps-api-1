9a730fb902a111d7f80dee89c80a2d76f7020e41
Josh Smith <joshdotsmith@gmail.com>
2016-10-08 16:37:33 -0700

    Convert user role except create

--- web/controllers/user_role_controller.ex
+++ web/controllers/user_role_controller.ex
@@ -2,20 +2,19 @@ defmodule CodeCorps.UserRoleController do
   @analytics Application.get_env(:code_corps, :analytics)
 
   use CodeCorps.Web, :controller
+  use JaResource
+
+  import CodeCorps.FilterHelpers, only: [id_filter: 2]
 
   alias CodeCorps.UserRole
 
   plug :load_resource, model: UserRole, only: [:show], preload: [:user, :role]
   plug :load_and_authorize_changeset, model: UserRole, only: [:create]
   plug :load_and_authorize_resource, model: UserRole, only: [:delete]
+  plug JaResource, except: [:create]
 
-  def index(conn, params) do
-    user_roles =
-      UserRole
-      |> UserRole.index_filters(params)
-      |> Repo.all
-
-    render(conn, "index.json-api", data: user_roles)
+  def filter(_conn, query, "id", id_list) do
+    query |> id_filter(id_list)
   end
 
   def create(conn, %{"data" => %{"type" => "user-role"}}) do
@@ -30,17 +29,5 @@ defmodule CodeCorps.UserRoleController do
         |> put_status(:unprocessable_entity)
         |> render(CodeCorps.ChangesetView, "error.json-api", changeset: changeset)
     end
-  end
-
-  def show(conn, %{"id" => _id}) do
-    render(conn, "show.json-api", data: conn.assigns.user_role)
-  end
-
-  def delete(conn, %{"id" => _id}) do
-    conn.assigns.user_role |> Repo.delete!
-
-    conn
-    |> @analytics.track(:removed, conn.assigns.user_role)
-    |> send_resp(:no_content, "")
   end
 end

efc24625d4c0c92e154f5b17557dca4806dbfe34
Josh Smith <joshdotsmith@gmail.com>
2016-10-08 13:20:59 -0700

    Convert organization controller

--- web/controllers/organization_controller.ex
+++ web/controllers/organization_controller.ex
@@ -1,60 +1,20 @@
 defmodule CodeCorps.OrganizationController do
   use CodeCorps.Web, :controller
+  use JaResource
 
   alias CodeCorps.Organization
-  alias JaSerializer.Params
 
-  import Organization, only: [changeset: 2]
+  import CodeCorps.ControllerHelpers, only: [coalesce_id_string: 1]
 
   plug :load_and_authorize_resource, model: Organization, only: [:create, :update]
-  plug :scrub_params, "data" when action in [:create, :update]
+  plug JaResource
 
-  def index(conn, params) do
-    organizations =
-      Organization
-      |> Organization.index_filters(params)
-      |> Repo.all
-
-    render(conn, "index.json-api", data: organizations)
+  def filter(_conn, query, "id", id_list) do
+    ids = id_list |> coalesce_id_string
+    query |> where([object], object.id in ^ids)
   end
 
-  def create(conn, %{"data" => data = %{"type" => "organization", "attributes" => _organization_params}}) do
-    changeset = Organization.create_changeset(%Organization{}, Params.to_attributes(data))
-
-    case Repo.insert(changeset) do
-      {:ok, organization} ->
-        conn
-        |> put_status(:created)
-        |> put_resp_header("location", organization_path(conn, :show, organization))
-        |> render("show.json-api", data: organization)
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(CodeCorps.ChangesetView, "error.json-api", changeset: changeset)
-    end
+  def handle_create(_conn, attributes) do
+    Organization.create_changeset(%Organization{}, attributes)
   end
-
-  def show(conn, %{"id" => id}) do
-    organization =
-      Organization
-      |> Repo.get!(id)
-    render(conn, "show.json-api", data: organization)
-  end
-
-  def update(conn, %{"id" => id, "data" => data = %{"type" => "organization", "attributes" => _organization_params}}) do
-    changeset =
-      Organization
-      |> Repo.get!(id)
-      |> changeset(Params.to_attributes(data))
-
-    case Repo.update(changeset) do
-      {:ok, organization} ->
-        render(conn, "show.json-api", data: organization)
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(CodeCorps.ChangesetView, "error.json-api", changeset: changeset)
-    end
-  end
-
 end
